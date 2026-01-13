489496d9def0d6c904a1bdb924f9a9b4e9f94318
Beni <bendzy.ali@gmail.com>
2026-01-13 16:57:57 +0100

    dsdas
    dasfa:wq
    
    sfdfd

--- src/main/java/com/beni/javaoop/Oseba.java
+++ src/main/java/com/beni/javaoop/Oseba.java
@@ -8,6 +8,61 @@ package com.beni.javaoop;
  *
  * @author bendz
  */
-public class Oseba {
+public abstract class Oseba {
+    
+    private String ime;
+    private String priimek;
+    private String EMSO;
+    private String email;
+    
+    
+    // GET & SET
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    private int letoRojstva;
+    private String telStevilka;
+
+    public String getIme() {
+        return ime;
+    }
+
+    public void setIme(String ime) {
+        this.ime = ime;
+    }
+
+    public String getPriimek() {
+        return priimek;
+    }
+
+    public void setPriimek(String priimek) {
+        this.priimek = priimek;
+    }
+
+    public String getEMSO() {
+        return EMSO;
+    }
+
+   
+    public int getLetoRojstva() {
+        return letoRojstva;
+    }
+
+    public void setLetoRojstva(int letoRojstva) {
+        this.letoRojstva = letoRojstva;
+    }
+
+    public String getTelStevilka() {
+        return telStevilka;
+    }
+
+    public void setTelStevilka(String telStevilka) {
+        this.telStevilka = telStevilka;
+    }
+    
     
 }
