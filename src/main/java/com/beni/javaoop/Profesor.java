ac6989ff62241574829fa230f7ebd7ba0c3c1d01
Beni <bendzy.thx@gmail.com>
2026-01-16 17:35:47 +0100

    Worked on Profesor class, need to fix izpisPodatkov

--- src/main/java/com/beni/javaoop/Profesor.java
+++ src/main/java/com/beni/javaoop/Profesor.java
@@ -4,15 +4,50 @@
  */
 package com.beni.javaoop;
 
+import java.util.List;
+import java.util.Objects;
+
 /**
  *
  * @author bendz
  */
 public class Profesor extends Oseba {
-
+    private String katedra;
+    private List<Predmet> predmeti;
+    private List<Student> studenti;
+    
+    public void dodajPredmet(Predmet predmet) {
+        Objects.requireNonNull(predmet," Predmet ne more biti null");
+        if (!this.predmeti.contains(predmet)) {
+            this.predmeti.add(predmet);
+        }else {
+            System.out.println("Ta predmet ze obstaja");
+        }
+    }
+    
     @Override
     public String getOpis() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+       
+        return "Profesor : "+this.getIme()+" "+
+                this.getPriimek()+" \nEMSO : "+
+                this.getEMSO()+" Katedra : "+this.katedra;
+    }
+    
+    public void getIzpisPodatkov() {
+        System.out.println("\t--- PREDMETI PROFESORJA---");
+        if (!this.predmeti.isEmpty()) {
+            for (Predmet p : predmeti) {
+                System.out.println(p.getImePredmeta() +" Koda predmeta +"+p.getKodaPredmeta());
+                for(Student s : studenti) {
+                    for( Predmet x : s.getPredmeti()) {
+                        if(p.equals(s.getPredmeti())) {
+                            System.out.println(s.getOpis());
+                            
+                        }
+                    }
+                }
+            }
+        }
     }
     
 }
